bf0bdfd3b02496c5a74f62e0d4165d17610117c8
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-03 10:50:26 +0100

    feat:report ccontroller

---
+++ lib/src/controllers/report_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/widgets.dart';
+
+
+class ReportController extends ChangeNotifier{
+  bool isAnonymous = false;
+  changeReportVisibility(){
+    //isAno
+  }
+}
