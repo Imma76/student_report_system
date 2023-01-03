5b13359c5a2846aa5a474149093504c16ba9a382
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-04 00:43:08 +0100

    feat :installed bot toast and sign in ccontrollers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bot_toast/bot_toast.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:report_system/src/providers/all_providers.dart';
