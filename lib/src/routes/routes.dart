d103ecdeaf268d6320a3e9f70f12ae7262f99acf
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-03 10:21:53 +0100

    report form route

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,10 +3,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:report_system/src/view/home/bottom_bar.dart';
 import 'package:report_system/src/view/home/home.dart';
+import 'package:report_system/src/view/reports/report_form.dart';
 import 'package:report_system/src/view/settings/settings.dart';
 
 Map<String,WidgetBuilder> routes ={
   HomePage.route:(BuildContext context)=>const HomePage(),
   BottomNavBar.route:(BuildContext context)=>const BottomNavBar(),
   Settings.route:(BuildContext context)=> const Settings(),
+  ReportForm.route:(BuildContext context)=> ReportForm(),
 };
