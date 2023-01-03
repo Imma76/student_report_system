e4956469f6eb52df54adaa3595d608c592defa94
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-03 10:51:37 +0100

    feat: added report provider

--- lib/src/providers/all_providers.dart
+++ lib/src/providers/all_providers.dart
@@ -1,9 +1,11 @@
 //
 import 'package:flutter/material.dart';
  import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:report_system/src/controllers/report_controller.dart';
 // import 'package:report_system/src/controllers/sign_up_controller.dart';
 //
  import '../utils/app_theme.dart';
 //
  final themeProvider= ChangeNotifierProvider<AppTheme>((ref)=>AppTheme());
+ final reportProvider = ChangeNotifierProvider<ReportController>((ref) => ReportController());
 // final signUpPovider = ChangeNotifierProvider<SignUpController>((ref) => SignUpController());
