8467415592bac78b3f17a6b07e53d27a84269c95
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-07 14:39:35 +0100

    refactoring and image ccontrollers

---
+++ lib/src/controllers/image_controller.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageController extends ChangeNotifier{
+
+  List<File> audioFiles = [];
+  List<File> imageFiles= List<File>.empty(growable: true);
+  List<File> videoFiles = [];
+  final _picker = ImagePicker();
+
+  void pickImage ()async{
+
+    try {
+      print('ksjshdhgd');
+      final pickedImageFiles = await _picker.pickMultiImage(
+        // source: ImageSource.gallery,
+      );
+
+
+
+      if (pickedImageFiles!.isNotEmpty) {
+        for (XFile image in pickedImageFiles) {
+          imageFiles.add(File(image.path));
+          notifyListeners();
+        }
+        // return ourImages;
+      } else {
+        print('selected image is null');
+        // return ourImages;
+      }
+    } catch (e) {
+      print('hhhh$e');
+
+    }
+  }
+  void pickVideos ()async{
+    FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true,type: FileType.video);
+
+    if (result != null) {
+      videoFiles = result.paths.map((path) => File(path.toString())).toList();
+      notifyListeners();
+    } else {
+      // User canceled the picker
+    }
+  }
+  void pickAudios ()async{
+    FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true,type: FileType.audio);
+
+    if (result != null) {
+      audioFiles = result.paths.map((path) => File(path.toString())).toList();
+      notifyListeners();
+    } else {
+      // User canceled the picker
+    }
+  }
+}
--- lib/src/controllers/report_controller.dart
+++ lib/src/controllers/report_controller.dart
@@ -14,60 +14,12 @@ class ReportController extends ChangeNotifier{
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController currentLevelController = TextEditingController();
-  final _picker = ImagePicker();
+
   changeReportVisibility(bool newValue){
     isAnonymous = newValue;
     notifyListeners();
   }
-  List<File> audioFiles = [];
-  List<File> imageFiles= List<File>.empty(growable: true);
-  List<File> videoFiles = [];
 
-  void pickImage ()async{
-
-    try {
-      print('ksjshdhgd');
-      final pickedImageFiles = await _picker.pickMultiImage(
-        // source: ImageSource.gallery,
-      );
-    
-
-
-      if (pickedImageFiles!.isNotEmpty) {
-        for (XFile image in pickedImageFiles) {
-          imageFiles.add(File(image.path));
-          notifyListeners();
-        }
-       // return ourImages;
-      } else {
-        print('selected image is null');
-       // return ourImages;
-      }
-    } catch (e) {
-      print('hhhh$e');
-
-    }
-  }
-  void pickVideos ()async{
-    FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true,type: FileType.video);
-
-    if (result != null) {
-      videoFiles = result.paths.map((path) => File(path.toString())).toList();
-      notifyListeners();
-    } else {
-      // User canceled the picker
-    }
-  }
-  void pickAudios ()async{
-    FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true,type: FileType.audio);
-
-    if (result != null) {
-      audioFiles = result.paths.map((path) => File(path.toString())).toList();
-      notifyListeners();
-    } else {
-      // User canceled the picker
-    }
-  }
   bool validateMedia(){
     if(imageFiles.isEmpty && videoFiles.isEmpty && audioFiles.isEmpty){
       return false;
@@ -77,7 +29,7 @@ class ReportController extends ChangeNotifier{
 
 
   Future submitReport()async{
-    
+
   }
 
 }
