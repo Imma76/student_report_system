de1dcc0151ba96881029e87356676e4e82b278cf
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-07 14:02:07 +0100

    feat:welcome screen

--- lib/src/view/welcome_screen.dart
+++ lib/src/view/welcome_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:report_system/src/utils/colors.dart';
+import 'package:report_system/src/view/authentication/sign_up.dart';
 
 
 
@@ -8,6 +12,47 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: (){
+                Navigator.pushNamed(context, SignUp.route);
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(left:20.0,right:20),
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  color:primaryColor,
+                  child: Center(child: Text('Sign up', style: GoogleFonts.lora(color: white, fontWeight: FontWeight.bold, fontSize: 18),)),
+                ),
+              ),
+            ),
+            Gap(20),
+            InkWell(
+              onTap: (){
+                Navigator.pushNamed(context, SignUp.route);
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(left:20.0,right:20),
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+
+                  decoration: BoxDecoration(
+                    color:white,
+                    border:Border.all(color: primaryColor)
+                  ),
+                  child: Center(child: Text('Sign in', style: GoogleFonts.lora(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
