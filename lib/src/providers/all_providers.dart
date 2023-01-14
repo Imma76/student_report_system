449d455028267275303e9c5dd5d1aeecdf8bc7de
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-14 08:06:25 +0100

    feat: init home provider

--- lib/src/providers/all_providers.dart
+++ lib/src/providers/all_providers.dart
@@ -5,9 +5,11 @@ import 'package:report_system/src/controllers/central_state.dart';
 import 'package:report_system/src/controllers/report_controller.dart';
  import 'package:report_system/src/controllers/sign_up_controller.dart';
 //
- import '../utils/app_theme.dart';
+ import '../controllers/home_controller.dart';
+import '../utils/app_theme.dart';
 //
  final themeProvider= ChangeNotifierProvider<AppTheme>((ref)=>AppTheme());
  final reportProvider = ChangeNotifierProvider<ReportController>((ref) => ReportController());
  final signUpPovider = ChangeNotifierProvider<SignUpController>((ref) => SignUpController());
  final centralStateProvider= ChangeNotifierProvider<CentralState>((ref) => CentralState());
+ final homeProvider = ChangeNotifierProvider<HomeController>((ref) => HomeController());
