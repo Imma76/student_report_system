122be089b83a521b99e5e256d798ef22931f2c04
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-16 20:55:22 +0100

    ui changes && home controller chnages

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -1,16 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:report_system/src/collections/collections.dart';
 import 'package:report_system/src/models/user.dart';
 import 'package:report_system/src/servicees/user_service.dart';
 
 class HomeController extends ChangeNotifier{
   UserService userService = UserService();
   UserModel? userModel;
-  void initializeApp()async{
-    final response =await  userService.getCurrentUser();
+
+
+  void initializeHome()async{
+    final response = await  userService.getCurrentUser();
     if(response != null){
       userModel = response;
       notifyListeners();
     }
+
+  }
+  Stream<QuerySnapshot> reportStream(){
+    return Collections.reportCollection.snapshots();
   }
 
 }
--- lib/src/view/home/home.dart
+++ lib/src/view/home/home.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:report_system/src/controllers/home_controller.dart';
 import 'package:report_system/src/providers/all_providers.dart';
 import 'package:report_system/src/utils/app_theme.dart';
 import 'package:report_system/src/utils/colors.dart';
@@ -49,77 +51,120 @@ class _HomePageState extends ConsumerState<HomePage> {
             padding: EdgeInsets.all(10),
             child:  Text('Make a report',style: appTheme.textStyle.copyWith(color:white),),decoration: BoxDecoration(color: primaryColor,borderRadius: BorderRadius.circular(20)),),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 200,
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('TOTAL REPORTS SUBMITTED',style: appTheme.textStyle.copyWith(fontWeight: FontWeight.bold,color: Colors.white,),),
-                        Gap(20),
-                        CircularPercentIndicator(
-                          radius: 40.0,
-                          lineWidth: 5.0,
+        body: StreamBuilder<QuerySnapshot>(
+          stream: homeController.reportStream(),
+          builder: (context, snapshot) {
+            final data = snapshot.data!.docs;
+           // if(snapshot.hasData) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return Center(child: CircularProgressIndicator(),);
 
-                          percent: 0.5,
-                          center: new Text("100"),
-                          progressColor: Colors.green,
+            }
+            if(snapshot.hasError){
+              return Center(child:Text('Unable to load data'));
+            }
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 200,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left:15.0),
+                        child: Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Center(
+                              child: Text('REPORTS SUMMARY',
+                                style: appTheme.textStyle.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,),),
+                            ),
+                            Gap(20),
+
+                            Row(
+                              crossAxisAlignment:CrossAxisAlignment.center,
+                              children: [
+                                CircleAvatar(
+                                  radius:75,
+                                  child: LiquidCircularProgressIndicator(
+                                    value: data.length/100, // Defaults to 0.5.
+                                    valueColor: AlwaysStoppedAnimation(Colors.black), // Defaults to the current Theme's accentColor.
+                                    backgroundColor: Colors.white, // Defaults to the current Theme's backgroundColor.
+                                    borderColor: primaryColor,
+                                    borderWidth: 5.0,
+                                    direction: Axis.vertical, // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
+                                    center: Column(
+                                      mainAxisAlignment:MainAxisAlignment.center,
+                                      children: [
+                                        Text('${data.length.toString()}',style: appTheme.textStyle.copyWith(color:Colors.black,fontSize: 9)),
+                                        Gap(5),
+                                        Text(' REPORTS SUBMITTED ',style: appTheme.textStyle.copyWith(color:Colors.black,fontSize: 9),),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Gap(40),
+                                Column(
+                                  children: [
+                                    Text('40'),
+                                    Gap(5),
+                                    Text('Reports Submitted',style: appTheme.textStyle.copyWith(color:Colors.black,fontSize: 9)),
+                                    Gap(12),
+                                    Text('20'),
+                                    Gap(5),
+                                    Text('Reports In Review',style: appTheme.textStyle.copyWith(color:Colors.black,fontSize: 9)),
+                                  ],
+                                ),
+
+                                ],
+                            ),
+
+
+                          ],
                         ),
-                      ],
+                      ),
+                      decoration: BoxDecoration(color: white,
+                          borderRadius: BorderRadius.only(bottomLeft: Radius
+                              .circular(10), bottomRight: Radius.circular(10))),
                     ),
-                //   CircleAvatar(
-                //     radius:50,
-                //     child: LiquidCircularProgressIndicator(
-                //     value: 50, // Defaults to 0.5.
-                //     valueColor: AlwaysStoppedAnimation(Colors.pink), // Defaults to the current Theme's accentColor.
-                //     backgroundColor: Colors.white, // Defaults to the current Theme's backgroundColor.
-                //     borderColor: Colors.red,
-                //     borderWidth: 5.0,
-                //
-                //     direction: Axis.vertical, // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
-                //     center: Text("50",style: appTheme.textStyle.copyWith(color:Colors.black),),
-                // ),
-                //   ),
+
+                    Gap(20),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Recents', style: appTheme.textStyle,),
+                    ),
+                    Gap(20),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListView.builder(
+                          itemCount: data.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text('Report submitted',
+                                    style: appTheme.textStyle,),
+                                ), Icon(Icons.report_outlined),
+                              ],
+                            );
+                          }
+                      ),
+                    ),
+
+
                   ],
                 ),
-                decoration: BoxDecoration(  color: primaryColor,borderRadius: BorderRadius.only(bottomLeft:Radius.circular(10),bottomRight: Radius.circular(10))),
-              ),
-
-              Gap(20),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Recents',style: appTheme.textStyle,),
-              ),
-               Gap(20),
-               Padding(
-                 padding: const EdgeInsets.all(8.0),
-                 child: ListView.builder(
-                   itemCount: 10,shrinkWrap: true,
-                   itemBuilder: (context,index) {
-                     return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('Report submitted', style: appTheme.textStyle,),
-                          ),  Icon(Icons.report_outlined),
-                        ],
-                      );
-                   }
-                 ),
-               ),
+              );
 
 
-            ],
-          ),
+
+          }
         ),
       ),
     );
